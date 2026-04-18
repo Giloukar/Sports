c50b793d6ebaeadfd49d89853ea285766848a5c1
Your Name <you@example.com>
2026-04-18 20:59:15 +0200

    Fix module aliases dans metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,21 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
-const config = {};
+const config = {
+  resolver: {
+    extraNodeModules: {
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@screens': path.resolve(__dirname, 'src/screens'),
+      '@navigation': path.resolve(__dirname, 'src/navigation'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@theme': path.resolve(__dirname, 'src/theme'),
+      '@types': path.resolve(__dirname, 'src/types'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
