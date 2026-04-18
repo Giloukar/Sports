dfdb59f065f3d87f2c5c0297db9da67ebbc33223
Your Name <you@example.com>
2026-04-18 21:12:06 +0200

    a

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,25 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+        alias: {
+          "@store": "./src/store",
+          "@services": "./src/services",
+          "@components": "./src/components",
+          "@screens": "./src/screens",
+          "@navigation": "./src/navigation",
+          "@hooks": "./src/hooks",
+          "@theme": "./src/theme",
+          "@types": "./src/types",
+          "@utils": "./src/utils",
+          "@constants": "./src/constants",
+        },
+      },
+    ],
+    "react-native-reanimated/plugin",
+  ],
 };
