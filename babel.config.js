84e7bc59c4d5bbc3edac91c9adbcf983f4b97afd
Your Name <you@example.com>
2026-04-18 20:47:01 +0200

    Fix reanimated 3.16.7 + babel config

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        alias: {
-          '@components': './src/components',
-          '@screens': './src/screens',
-          '@services': './src/services',
-          '@store': './src/store',
-          '@hooks': './src/hooks',
-          '@utils': './src/utils',
-          '@types': './src/types',
-          '@constants': './src/constants',
-          '@theme': './src/theme',
-          '@navigation': './src/navigation',
-        },
-      },
-    ],
-    'react-native-reanimated/plugin',
-  ],
+  plugins: ['react-native-reanimated/plugin'],
 };
